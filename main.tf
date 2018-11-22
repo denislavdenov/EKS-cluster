60e6dee606abb5f1cf8f5d7baebabb83445e8e90
Denov <denov@Denovs-MacBook-Pro.local>
2018-11-22 13:47:36 +0200

    edit scri[ts

--- main.tf
+++ main.tf
@@ -9,42 +9,42 @@ resource "null_resource" "local_install" {
 
 }
 
-provider "kubernetes" {}
+#provider "kubernetes" {}
 
-resource "kubernetes_replication_controller" "example" {
+#resource "kubernetes_replication_controller" "example" {
+#
+ # depends_on = ["null_resource.local_install"]
+#
+ # metadata {
+  #  name = "terraform-example"
+#
+ #   labels {
+  #    test = "MyExampleApp"
+   # }
+  #}
 
-  depends_on = ["null_resource.local_install"]
-
-  metadata {
-    name = "terraform-example"
-
-    labels {
-      test = "MyExampleApp"
-    }
-  }
-
-  spec {
-    selector {
-      test = "MyExampleApp"
-    }
-
-    template {
-      container {
-        image = "nginx:1.7.8"
-        name  = "example"
-
-        resources {
-          limits {
-            cpu    = "0.5"
-            memory = "512Mi"
-          }
-
-          requests {
-            cpu    = "250m"
-            memory = "50Mi"
-          }
-        }
-      }
-    }
-  }
-}
+#  spec {
+#    selector {
+ #     test = "MyExampleApp"
+  #  }
+#
+ #   template {
+  #    container {
+   #     image = "nginx:1.7.8"
+    #    name  = "example"
+#
+ #       resources {
+  #        limits {
+   #         cpu    = "0.5"
+    #        memory = "512Mi"
+     #     }
+#
+ #         requests {
+  #          cpu    = "250m"
+   #         memory = "50Mi"
+    #      }
+     #   }
+      #}
+    #}
+  #}
+#}
