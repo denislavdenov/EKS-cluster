3f0eda7cb9d1153e3b29b34413a41e1566ddefca
Denislav Denov <denislavdenov@gmail.com>
2019-10-30 14:26:33 +0200

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -14,7 +14,7 @@ sudo curl -o /usr/local/bin/kubectl https://amazon-eks.s3-us-west-2.amazonaws.co
 sudo chmod +x /usr/local/bin/kubectl
 
 # from https://docs.aws.amazon.com/eks/latest/userguide/configure-kubectl.html
-sudo curl -o /usr/local/bin/aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/linux/amd64/aws-iam-authenticator
+sudo curl -o /usr/local/bin/aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
 sudo chmod +x /usr/local/bin/aws-iam-authenticator
 
 
