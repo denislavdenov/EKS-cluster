39b51240a087381889794e2582382ed1ddfb3f42
Denislav Denov <denislavdenov@gmail.com>
2019-10-30 16:00:42 +0200

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -4,15 +4,6 @@ set -x
 
 #exec 5>&1 &>/dev/null
 
-# from https://docs.aws.amazon.com/cli/latest/userguide/awscli-install-bundle.html
-# curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-# unzip awscli-bundle.zip
-# rm awscli-bundle.zip
-# sudo /usr/bin/python2 awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-# rm -fr awscli-bundle
-
-
-
 # from https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html#install-kubectl-linux
 curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/linux/amd64/kubectl
 chmod +x kubectl
